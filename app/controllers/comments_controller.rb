a2dd511700351884e82ab30f392e9f33a21892a1
ikedayohei <yoheii0612@gmail.com>
2020-08-14 21:24:01 +0900

    comment params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,12 @@
 class CommentsController < ApplicationController
+
   def create
   @comment = Comment.new(comment_params)
   end
-  
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, report_id: params[:report_id])
+  end
+
 end
