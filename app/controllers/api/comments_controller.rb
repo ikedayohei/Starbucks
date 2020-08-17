105a2317a934b20071fa18741adc204b9d0728ab
ikedayohei <yoheii0612@gmail.com>
2020-08-17 21:39:33 +0900

    api add

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,7 @@
+class Api::CommentsController < ApplicationController
+  def show
+    @review = Review.find(params[:review_id])
+    last_comment_id = params[:id].to_i
+    @comments = @review.comments.includes(:user).where("id > ?", last_comment_id)
+  end
+end
