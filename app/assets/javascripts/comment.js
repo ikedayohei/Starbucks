ae448fa5ae41a6d74e6ac0c15123ddd6574fe176
ikedayohei <yoheii0612@gmail.com>
2020-08-18 21:45:32 +0900

    auto-comment.js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -20,58 +20,6 @@ $(document).ready(function(){
                   
     return html;
   }
-    var reloadMessages = function() {
-      var last_comment_id = $('.commentText__list:last').data("comment-id");
-      let review_id = $(".commentText").data("review-id");
-    $.ajax({
-      url: `${review_id}/api/comments`,
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_comment_id}
-    })
-    .done(function(comments) {
-      console.log("test3")
-      if (comments.length !== 0) {
-        var insertHTML = '';
-        $.each(comments, function(i, comment) {
-          insertHTML += buildHTML(comment)
-        });
-        $('.commentText').append(insertHTML);
-        $('.commentText__list').animate({ scrollTop: $('.commentText__list')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
-    if (window.location.href.match(/\/reviews\/\d+/)) {
-      if (!window.location.href.match(/\/reviews\/\d+\/edit/)) {
-         setInterval(reloadMessages, 7000);
-      }
-    }
-  };
-})
-$(document).ready(function(){
-  function buildHTML(comment){
-    var html = 
-      `<div class="commentText__list" data-comment-id=${comment.id}>
-        <div class="commentText__list__icon">
-          <i class="fa fa-user"></i>
-        </div>
-        <div class="commentText__list__content">
-          <div class="commentText__list__content__name">
-            ${comment.user_name}
-          </div>
-          <div class="commentText__list__content__message">
-            ${comment.text}
-          </div>
-          <div class="commentText__list__content__date">
-            ${comment.created_at}
-          </div>
-        </div>
-      </div>`
-                  
-    return html;
-  }
   
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
