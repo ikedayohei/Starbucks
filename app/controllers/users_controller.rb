61f0ddfc6794abf65f63e46f7691ce8833b2909a
ikedayohei <yoheii0612@gmail.com>
2020-08-20 18:44:03 +0900

    show 変数追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only:[:show,:edit,:update,:destory]
   
   def show
-    
+    @number = @user.reviews.count(:id)
   end
   
   
