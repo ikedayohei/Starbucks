73f32b13caf88a41f1f043632ef1265fd3c3d714
ikedayohei <yoheii0612@gmail.com>
2020-08-21 18:28:35 +0900

    created at

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   
   def show
     @number = @user.reviews.count(:id)
-    @reviews = @user.reviews.page(params[:page]).per(6)
+    @reviews = @user.reviews.order('created_at DESC').page(params[:page]).per(6)
   end
   
   
