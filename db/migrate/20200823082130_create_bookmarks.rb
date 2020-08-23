f474e49c0dc7b8c078273bb0c2e5a986c658ecac
ikedayohei <yoheii0612@gmail.com>
2020-08-23 17:24:40 +0900

    一意性

--- db/migrate/20200823082130_create_bookmarks.rb
+++ db/migrate/20200823082130_create_bookmarks.rb
@@ -5,6 +5,7 @@ class CreateBookmarks < ActiveRecord::Migration[5.2]
       t.references :review, foreign_key: true
 
       t.timestamps
+      t.index [:user_id, :review_id], unique: true 
     end
   end
 end
