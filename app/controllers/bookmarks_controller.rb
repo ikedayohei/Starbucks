fc8b58e694e3da5b5126251e0f1fa56e4cbd6695
ikedayohei <yoheii0612@gmail.com>
2020-08-23 17:33:04 +0900

    conrtoller change

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,12 @@
 class BookmarksController < ApplicationController
   def create
+    bookmark = current_user.bookmarks.build(review_id: params[:review_id])
+    bookmark.save!
+    redirect_to root_path, success: t('.flash.bookmark')
   end
 
   def destroy
+    current_user.bookmarks.find_by(board_id: params[:board_id]).destroy!
+    redirect_to boards_path, success: t('.flash.not_bookmark')
   end
 end
