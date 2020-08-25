b59fc5d013d56475707372f910e1e255e753d776
ryota828 <ry0ta256@yahoo.co.jp>
2020-08-25 17:06:22 +0900

    Revert "Update unicorn.rb"
    This reverts commit e2173a534a53072cd0c18b494d62f3a23e15313f.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #ポート番号を指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen 3000
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
