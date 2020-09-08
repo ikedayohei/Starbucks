20dc491018bdacd101eb21767a12921936313d8c
ryota828 <ry0ta256@yahoo.co.jp>
2020-09-08 21:51:46 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,8 +13,8 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
-  }
-
+    }
   config.fog_directory  = 'starbucks-o67'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/starbucks-o67'
+  end
 end
