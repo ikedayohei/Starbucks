9ba20473067b8f7e6638f7262f689c0f5a4cc21c
ikedayohei <yoheii0612@gmail.com>
2020-09-10 18:20:46 +0900

    change

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,19 +1,14 @@
 class BookmarksController < ApplicationController
-  before_action :set_review, only: [:create,:destroy]
-
-
+  
   def create
     bookmark = current_user.bookmarks.build(review_id: params[:review_id])
     bookmark.save!
-    redirect_to review_path(@review), success: t('.flash.bookmark')
+    redirect_to root_path, success: t('.flash.bookmark')
   end
 
   def destroy
     current_user.bookmarks.find_by(review_id: params[:review_id]).destroy!
-    redirect_to review_path(@review), success: t('.flash.not_bookmark')
+    redirect_to root_path, success: t('.flash.not_bookmark')
   end
 
-  def set_review
-    @review =Review.find(params[:id])
-  end
 end
