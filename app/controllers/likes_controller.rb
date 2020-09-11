1a90978d61e803d915555519e3b83bf3b2c9350e
ikedayohei <yoheii0612@gmail.com>
2020-09-11 18:37:52 +0900

    css change

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,6 @@
 class LikesController < ApplicationController
   before_action :set_review
-  
+
   def create
     @like = Like.create(user_id: current_user.id, review_id: params[:review_id])
     @likes = current_user.likes
