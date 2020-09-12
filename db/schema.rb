0735275c758da48f01583907b39a31ab1d31e508
ikedayohei <yoheii0612@gmail.com>
2020-09-12 12:27:01 +0900

    migrate change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_094148) do
+ActiveRecord::Schema.define(version: 2020_09_12_032606) do
 
   create_table "bookmarks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_09_10_094148) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_count"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
